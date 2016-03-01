37ddc9eb58b7be85d235814dafff976524d8ce9e
Bryan Sugiarto <bsugiarto@bsugiarto-ltm.internal.salesforce.com>
2016-02-29 21:09:09 -0800

    auto push

--- script2.sh
+++ script2.sh
@@ -2,7 +2,7 @@
 
 make clean
 make activity
-hadoop jar activity.jar -libjars org.json-20120521.jar,json-mapreduce-1.0.jar example.txt test/output/
+hadoop jar activity.jar -libjars org.json-20120521.jar,json-mapreduce-1.0.jar out.txt test/output/
 make get
 make show
 
