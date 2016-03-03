4d962d96b3bfadb0cdc057d42e25165c734174d7
Bryan Sugiarto <bsugiarto@bsugiarto-ltm.internal.salesforce.com>
2016-03-03 15:22:45 -0800

    auto push

--- beFuddledGen.java
+++ beFuddledGen.java
@@ -21,7 +21,7 @@ public class beFuddledGen {
 	
 	public static void main(String[] args) {
 		int NUMBEROFGAMES = 20;
-		int obj = 1000;
+		int obj = 10000;
 		String file = "out.txt";
 		
 		System.out.println("obj: " + obj);
